d067b5a2a35341b8db3e7a60f8f2372bcbfc2985
69bruno <67907472+69bruno@users.noreply.github.com>
2026-03-26 08:11:54 +0100

    Update v102_acp_modules.php

--- migrations/v102_acp_modules.php
+++ migrations/v102_acp_modules.php
@@ -1,1 +1,51 @@
+<?php
+namespace bruno\mopedgarage\migrations;
 
+class v102_acp_modules extends \phpbb\db\migration\migration
+{
+	public static function depends_on()
+	{
+		return ['\bruno\mopedgarage\migrations\v101_permissions'];
+	}
+
+	public function update_data()
+	{
+		return [
+			['module.add', [
+				'acp',
+				'ACP_CAT_DOT_MODS',
+				[
+					'module_langname'	=> 'ACP_MOPEDGARAGE',
+					'module_enabled'	=> 1,
+					'module_display'	=> 1,
+				],
+			]],
+
+			['module.add', [
+				'acp',
+				'ACP_MOPEDGARAGE',
+				[
+					'module_basename'	=> '\bruno\mopedgarage\acp\acp_mopedgarage_module',
+					'module_langname'	=> 'ACP_MOPEDGARAGE_SETTINGS',
+					'module_mode'		=> 'settings',
+					'module_auth'		=> 'acl_a_mopedgarage_manage',
+					'module_enabled'	=> 1,
+					'module_display'	=> 1,
+				],
+			]],
+
+			['module.add', [
+				'acp',
+				'ACP_MOPEDGARAGE',
+				[
+					'module_basename'	=> '\bruno\mopedgarage\acp\acp_mopedgarage_module',
+					'module_langname'	=> 'ACP_MOPEDGARAGE_FIELDS',
+					'module_mode'		=> 'fields',
+					'module_auth'		=> 'acl_a_mopedgarage_manage',
+					'module_enabled'	=> 1,
+					'module_display'	=> 1,
+				],
+			]],
+		];
+	}
+}
