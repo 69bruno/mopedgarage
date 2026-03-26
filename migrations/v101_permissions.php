9c88429191c29bf863bdee2b571bd987e4735fce
69bruno <67907472+69bruno@users.noreply.github.com>
2026-03-26 08:11:04 +0100

    Update v101_permissions.php

--- migrations/v101_permissions.php
+++ migrations/v101_permissions.php
@@ -1,1 +1,39 @@
+<?php
+namespace bruno\mopedgarage\migrations;
 
+class v101_permissions extends \phpbb\db\migration\migration
+{
+	public static function depends_on()
+	{
+		return ['\bruno\mopedgarage\migrations\v100'];
+	}
+
+	public function update_data()
+	{
+		return [
+			['permission.add', ['u_mopedgarage_view']],
+			['permission.add', ['u_mopedgarage_use']],
+			['permission.add', ['a_mopedgarage_manage']],
+
+			['permission.permission_set', [
+				'REGISTERED',
+				['u_mopedgarage_view', 'u_mopedgarage_use'],
+				'group',
+			]],
+			['permission.permission_set', [
+				'ADMINISTRATORS',
+				['u_mopedgarage_view', 'u_mopedgarage_use', 'a_mopedgarage_manage'],
+				'group',
+			]],
+		];
+	}
+
+	public function revert_data()
+	{
+		return [
+			['permission.remove', ['u_mopedgarage_view']],
+			['permission.remove', ['u_mopedgarage_use']],
+			['permission.remove', ['a_mopedgarage_manage']],
+		];
+	}
+}
