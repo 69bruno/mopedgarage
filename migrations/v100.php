d5d954e00556f132096db0d729fa793fbe3af287
69bruno <67907472+69bruno@users.noreply.github.com>
2026-03-26 08:08:23 +0100

    new sorted migrations

--- migrations/v100.php
+++ migrations/v100.php
@@ -3,23 +3,23 @@ namespace bruno\mopedgarage\migrations;
 
 class v100 extends \phpbb\db\migration\migration
 {
+	public static function depends_on()
+	{
+		return ['\phpbb\db\migration\data\v330\v330'];
+	}
+
 	public function effectively_installed()
 	{
 		return isset($this->config['mopedgarage_version'])
 			&& version_compare($this->config['mopedgarage_version'], '1.0.0', '>=');
 	}
 
-	public static function depends_on()
-	{
-		return ['\phpbb\db\migration\data\v330\v330'];
-	}
-
 	public function update_schema()
 	{
 		return [
-			'add_tables' => [
-				$this->table_prefix . 'mopedgarage_bikes' => [
-					'COLUMNS' => [
+			'add_tables'	=> [
+				$this->table_prefix . 'mopedgarage_bikes'	=> [
+					'COLUMNS'		=> [
 						'id'			=> ['UINT', null, 'auto_increment'],
 						'user_id'		=> ['UINT', 0],
 						'brand'			=> ['VCHAR:100', ''],
@@ -32,7 +32,7 @@ class v100 extends \phpbb\db\migration\migration
 						'updated_at'	=> ['UINT', 0],
 					],
 					'PRIMARY_KEY'	=> 'id',
-					'KEYS' => [
+					'KEYS'			=> [
 						'user_id'			=> ['INDEX', 'user_id'],
 						'brand'				=> ['INDEX', 'brand'],
 						'model'				=> ['INDEX', 'model'],
@@ -42,8 +42,8 @@ class v100 extends \phpbb\db\migration\migration
 					],
 				],
 
-				$this->table_prefix . 'mopedgarage_fields' => [
-					'COLUMNS' => [
+				$this->table_prefix . 'mopedgarage_fields'	=> [
+					'COLUMNS'		=> [
 						'field_id'				=> ['UINT', null, 'auto_increment'],
 						'field_name'			=> ['VCHAR:50', ''],
 						'field_label'			=> ['VCHAR:255', ''],
@@ -59,21 +59,21 @@ class v100 extends \phpbb\db\migration\migration
 						'created_at'			=> ['UINT', 0],
 					],
 					'PRIMARY_KEY'	=> 'field_id',
-					'KEYS' => [
+					'KEYS'			=> [
 						'field_name'	=> ['UNIQUE', 'field_name'],
 						'field_sort'	=> ['INDEX', 'field_sort'],
 					],
 				],
 
-				$this->table_prefix . 'mopedgarage_field_values' => [
-					'COLUMNS' => [
+				$this->table_prefix . 'mopedgarage_field_values'	=> [
+					'COLUMNS'		=> [
 						'bike_id'		=> ['UINT', 0],
 						'field_id'		=> ['UINT', 0],
 						'field_value'	=> ['VCHAR:255', ''],
 					],
 					'PRIMARY_KEY'	=> ['bike_id', 'field_id'],
-					'KEYS' => [
-						'field_id'	=> ['INDEX', 'field_id'],
+					'KEYS'			=> [
+						'field_id'		=> ['INDEX', 'field_id'],
 					],
 				],
 			],
@@ -83,7 +83,7 @@ class v100 extends \phpbb\db\migration\migration
 	public function revert_schema()
 	{
 		return [
-			'drop_tables' => [
+			'drop_tables'	=> [
 				$this->table_prefix . 'mopedgarage_field_values',
 				$this->table_prefix . 'mopedgarage_fields',
 				$this->table_prefix . 'mopedgarage_bikes',
@@ -106,63 +106,6 @@ class v100 extends \phpbb\db\migration\migration
 			['config.add', ['mopedgarage_enable_gallery', 1]],
 			['config.add', ['mopedgarage_lightbox_global', 1]],
 			['config.add', ['mopedgarage_mobile_card_scale', 'compact']],
-
-			['permission.add', ['u_mopedgarage_view']],
-			['permission.add', ['u_mopedgarage_use']],
-			['permission.add', ['a_mopedgarage_manage']],
-
-			['permission.permission_set', ['REGISTERED', ['u_mopedgarage_view', 'u_mopedgarage_use'], 'group']],
-			['permission.permission_set', ['ADMINISTRATORS', ['u_mopedgarage_view', 'u_mopedgarage_use', 'a_mopedgarage_manage'], 'group']],
-
-			['module.add', [
-				'acp',
-				'ACP_CAT_DOT_MODS',
-				[
-					'module_langname'	=> 'ACP_MOPEDGARAGE',
-					'module_enabled'	=> 1,
-					'module_display'	=> 1,
-				],
-			]],
-
-			['module.add', [
-				'acp',
-				'ACP_MOPEDGARAGE',
-				[
-					'module_basename'	=> '\bruno\mopedgarage\acp\acp_mopedgarage_module',
-					'module_langname'	=> 'ACP_MOPEDGARAGE_SETTINGS',
-					'module_mode'		=> 'settings',
-					'module_auth'		=> 'acl_a_mopedgarage_manage',
-					'module_enabled'	=> 1,
-					'module_display'	=> 1,
-				],
-			]],
-
-			['module.add', [
-				'acp',
-				'ACP_MOPEDGARAGE',
-				[
-					'module_basename'	=> '\bruno\mopedgarage\acp\acp_mopedgarage_module',
-					'module_langname'	=> 'ACP_MOPEDGARAGE_FIELDS',
-					'module_mode'		=> 'fields',
-					'module_auth'		=> 'acl_a_mopedgarage_manage',
-					'module_enabled'	=> 1,
-					'module_display'	=> 1,
-				],
-			]],
-
-			['module.add', [
-				'ucp',
-				'UCP_PROFILE',
-				[
-					'module_basename'	=> '\bruno\mopedgarage\ucp\ucp_mopedgarage_module',
-					'module_langname'	=> 'UCP_MOPEDGARAGE_EDIT',
-					'module_mode'		=> 'edit',
-					'module_auth'		=> 'acl_u_mopedgarage_use',
-					'module_enabled'	=> 1,
-					'module_display'	=> 1,
-				],
-			]],
-
 			['config.add', ['mopedgarage_version', '1.0.0']],
 		];
 	}
@@ -183,10 +126,6 @@ class v100 extends \phpbb\db\migration\migration
 			['config.remove', ['mopedgarage_lightbox_global']],
 			['config.remove', ['mopedgarage_mobile_card_scale']],
 			['config.remove', ['mopedgarage_version']],
-
-			['permission.remove', ['u_mopedgarage_view']],
-			['permission.remove', ['u_mopedgarage_use']],
-			['permission.remove', ['a_mopedgarage_manage']],
 		];
 	}
 }
