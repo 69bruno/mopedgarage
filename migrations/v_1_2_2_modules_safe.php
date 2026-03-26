91b3dc6383906cd8674a2c1475493ccb78914ece
69bruno <67907472+69bruno@users.noreply.github.com>
2026-03-26 07:32:56 +0100

    bug fix

--- migrations/v_1_2_2_modules_safe.php
+++ migrations/v_1_2_2_modules_safe.php
@@ -10,8 +10,8 @@ class v_1_2_2_modules_safe extends \phpbb\db\migration\migration
 
         if ($parent_langname !== null)
         {
-            $sql .= ' LEFT JOIN ' . MODULES_TABLE . " p
-                ON p.module_id = m.parent_id";
+            $sql .= ' LEFT JOIN ' . MODULES_TABLE . ' p
+                ON p.module_id = m.parent_id';
         }
 
         $sql .= " WHERE m.module_class = '" . $this->db->sql_escape($module_class) . "'
@@ -31,56 +31,72 @@ class v_1_2_2_modules_safe extends \phpbb\db\migration\migration
 
     public function effectively_installed()
     {
-        return $this->mg_module_exists('acp', 'ACP_MOPEDGARAGE');
+        return $this->mg_module_exists('acp', 'ACP_MOPEDGARAGE')
+            && $this->mg_module_exists('acp', 'ACP_MOPEDGARAGE_SETTINGS', 'ACP_MOPEDGARAGE')
+            && $this->mg_module_exists('acp', 'ACP_MOPEDGARAGE_FIELDS', 'ACP_MOPEDGARAGE')
+            && $this->mg_module_exists('ucp', 'UCP_MOPEDGARAGE_EDIT');
     }
 
     public function update_data()
     {
-        return [
-            ['custom', [[$this, 'install_modules']]],
-        ];
-    }
+        $data = [];
 
-    public function install_modules()
-    {
         if (!$this->mg_module_exists('acp', 'ACP_MOPEDGARAGE'))
         {
-            $this->module_manager->add_module('acp', 0, [
-                'module_basename' => '\bruno\mopedgarage\acp\main_module',
-                'module_langname' => 'ACP_MOPEDGARAGE',
-                'module_mode'     => '',
-                'module_auth'     => 'acl_a_mopedgarage_manage',
-            ]);
+            $data[] = ['module.add', [
+                'acp',
+                0,
+                [
+                    'module_basename' => '\bruno\mopedgarage\acp\main_module',
+                    'module_langname' => 'ACP_MOPEDGARAGE',
+                    'module_mode'     => '',
+                    'module_auth'     => 'ext_bruno/mopedgarage && acl_a_mopedgarage_manage',
+                ],
+            ]];
         }
 
         if (!$this->mg_module_exists('acp', 'ACP_MOPEDGARAGE_SETTINGS', 'ACP_MOPEDGARAGE'))
         {
-            $this->module_manager->add_module('acp', 'ACP_MOPEDGARAGE', [
-                'module_basename' => '\bruno\mopedgarage\acp\main_module',
-                'module_langname' => 'ACP_MOPEDGARAGE_SETTINGS',
-                'module_mode'     => 'settings',
-                'module_auth'     => 'acl_a_mopedgarage_manage',
-            ]);
+            $data[] = ['module.add', [
+                'acp',
+                'ACP_MOPEDGARAGE',
+                [
+                    'module_basename' => '\bruno\mopedgarage\acp\main_module',
+                    'module_langname' => 'ACP_MOPEDGARAGE_SETTINGS',
+                    'module_mode'     => 'settings',
+                    'module_auth'     => 'ext_bruno/mopedgarage && acl_a_mopedgarage_manage',
+                ],
+            ]];
         }
 
         if (!$this->mg_module_exists('acp', 'ACP_MOPEDGARAGE_FIELDS', 'ACP_MOPEDGARAGE'))
         {
-            $this->module_manager->add_module('acp', 'ACP_MOPEDGARAGE', [
-                'module_basename' => '\bruno\mopedgarage\acp\main_module',
-                'module_langname' => 'ACP_MOPEDGARAGE_FIELDS',
-                'module_mode'     => 'fields',
-                'module_auth'     => 'acl_a_mopedgarage_manage',
-            ]);
+            $data[] = ['module.add', [
+                'acp',
+                'ACP_MOPEDGARAGE',
+                [
+                    'module_basename' => '\bruno\mopedgarage\acp\main_module',
+                    'module_langname' => 'ACP_MOPEDGARAGE_FIELDS',
+                    'module_mode'     => 'fields',
+                    'module_auth'     => 'ext_bruno/mopedgarage && acl_a_mopedgarage_manage',
+                ],
+            ]];
         }
 
         if (!$this->mg_module_exists('ucp', 'UCP_MOPEDGARAGE_EDIT'))
         {
-            $this->module_manager->add_module('ucp', 0, [
-                'module_basename' => '\bruno\mopedgarage\ucp\main_module',
-                'module_langname' => 'UCP_MOPEDGARAGE_EDIT',
-                'module_mode'     => 'edit',
-                'module_auth'     => 'acl_u_mopedgarage_use',
-            ]);
+            $data[] = ['module.add', [
+                'ucp',
+                0,
+                [
+                    'module_basename' => '\bruno\mopedgarage\ucp\main_module',
+                    'module_langname' => 'UCP_MOPEDGARAGE_EDIT',
+                    'module_mode'     => 'edit',
+                    'module_auth'     => 'ext_bruno/mopedgarage && acl_u_mopedgarage_use',
+                ],
+            ]];
         }
+
+        return $data;
     }
 }
